b7c4fc2420c20ee413b122cab27d887f1b4ea189
JouskaPram <pramudita5800@gmail.com>
2023-07-07 17:30:31 +0700

    :v

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,28 @@
-<h1 style="text-align: center">Ala Bloon^^</h1>
-<p>ci cd nya berhasil cuy</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .flex{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex">
+        <button onclick="button()">Click Me^^</button>
+    </div>
+    <script>
+        function button(){
+            alert("Ala bloon")
+        }
+    </script>
+</body>
+</html>
