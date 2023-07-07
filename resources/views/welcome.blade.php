c886d58fa02052fbd71f77057574abac09124c4c
JouskaPram <pramudita5800@gmail.com>
2023-07-07 16:28:00 +0700

    test ci cd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,2 @@
 <h1 style="text-align: center">Ala Bloon^^</h1>
-<p>live ci cd after debug yang entah gak tau berapa kaliiii</p>
+<p>live ci cd after debug yang entah gak tau berapa kaliiiinya:v</p>
