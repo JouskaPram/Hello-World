edc00b3c7ab59af15e8b0bcdd2bb17f0cc6a0ee0
JouskaPram <pramudita5800@gmail.com>
2023-07-14 11:32:30 +0700

    new kuru kuru design

--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -1,18 +1,46 @@
 <template>
-    <div>
-        <div class="w-full h-full min-h-screen bg-purple-600">
-            <div class="flex h-screen w-full align-middle items-center justify-center ">
-                <div class="align-middle items-center justify-center ">
-                    <h1 class="animated text-center text-white fw-bold text-3xl" v-if="loading == true">Tunggu Sebentar^^
+    <div class="w-full h-screen pb-5 md:pb-0 bg-[#9d88d3] ">
+
+
+        <div class=" items-center px-16 pram h-[100vh]">
+            <div class="image-container">
+
+                <transition name="slide-in" mode="out-in">
+                    <img v-if="show" src="https://herta.eu.org/img/hertaa2.webp" class="slide-in-image ">
+                </transition>
+            </div>
+
+
+            <div class="relative flex py-5 items-center">
+                <div class="flex-grow border-t border-white"></div>
+                <span class="flex-shrink mx-4 text-white md:text-3xl text-xl">Got Your Kuru Kuru</span>
+                <div class="flex-grow border-t border-white"></div>
+            </div>
+            <h2 class="text-center text-white ">The Website Just For Fun</h2>
+
+            <div class="flex h-[60vh] w-full align-middle items-center justify-center">
+
+                <div class="align-middle items-center justify-center">
+
+                    <h4 class=" text-center text-[#574f84] fw-bold text-lg">The Kuru~ Squished</h4>
+                    <h1 class=" text-center text-white fw-bold text-3xl"
+                        :class="{ 'scale-110 animate-spin duration-500': animate }" v-if="!loading">
+
+                        {{ counter }}
+                        <br>
+                        <span>Times</span>
                     </h1>
-                    <h1 class="animated text-center text-white fw-bold text-3xl"
-                        :class="{ 'scale-110 animate-spin duration-500': animate }" v-if="!loading"> {{
-                            counter
-                        }}</h1>
-                    <button @click="incrementTempCounter"
-                        class="py-2 px-5 text-2xl fw-bold bg-purple-400 rounded-md mt-3 text-white">Kuru
-                        Kuru</button>
+                    <button @click="incrementCounter"
+                        class="py-2 px-5 text-2xl fw-bold bg-[#574f84] rounded-lg mt-3 text-white hover:scale-105 hover:opacity-90">
+                        Squisy Here!!
+                    </button>
                 </div>
+            </div>
+
+            <div class="relative flex py-5 items-center">
+                <div class="flex-grow border-t border-white"></div>
+                <span class="flex-shrink mx-4 text-white md:text-3xl text-xl">Got Your Kuru Kuru</span>
+                <div class="flex-grow border-t border-white"></div>
             </div>
         </div>
     </div>
@@ -20,86 +48,106 @@
 
 <script>
 import { ref, onMounted } from 'vue';
-import Pusher from 'pusher-js';
-import Echo from 'laravel-echo';
 
 export default {
     setup() {
-        const tempCounter = ref(0);
-        const counter = ref();
-        const totalClicks = ref(0);
-        const loading = ref(true)
-        let animate = ref(false);
-        const pusherChannel = ref(null);
-        let pendingUpdate = null;
+        const counter = ref(0);
+        const show = ref(false);
+        const images = ref([
+            'https://herta.eu.org/img/hertaa2.webp',
+            'https://herta.eu.org/img/hertaa1.webp'
+        ]);
 
-
-
-        const updateCounterOnServer = async () => {
-            // getCounterFromServer()
-
-            // Send the counter value to the server
-            await axios.post('/api/increment', {
-                "count": totalClicks.value,
-            }).then((response) => {
-                console.log(response)
-            });
-            console.log(totalClicks)
-
-            // Reset the temporary counter
-            tempCounter.value = 0;
-            totalClicks.value = 0;
-
-
-        };
-
-
-        const incrementTempCounter = () => {
-            animate.value = true;
-            tempCounter.value += 1;
+        const incrementCounter = () => {
+            show.value = true;
             counter.value += 1;
-            totalClicks.value += 1;
-
-
-            if (pendingUpdate !== null) {
-                // If there is a pending update, clear it
-                clearTimeout(pendingUpdate);
-            };
-
-            pendingUpdate = setTimeout(updateCounterOnServer, 5000);
-            animate.value = false;
-
+            localStorage.setItem('counter', counter.value.toString());
+            setTimeout(() => {
+                show.value = false;
+            }, 1000);
         };
 
-        const getCounterFromServer = () => {
-            loading.value = true
-            animate.value = false;
-            axios
-                .get('/api/counter')
-                .then((response) => {
-                    counter.value = response.data.count;
-                })
+        const getCounterFromStorage = () => {
+            const savedCounter = localStorage.getItem('counter');
+            if (savedCounter !== null) {
+                counter.value = parseInt(savedCounter);
+            }
 
-            loading.value = false
+
         };
 
         onMounted(() => {
-            getCounterFromServer();
-
+            getCounterFromStorage();
         });
 
         return {
             counter,
-            animate,
-            totalClicks,
-            incrementTempCounter,
+            show,
+            images,
+            incrementCounter
         };
-    },
+    }
 };
 </script>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lilita+One&family=Poppins&display=swap');
+
+.pram {
+    font-family: 'Lilita One', cursive;
 
 
+}
+
+body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.wave-svg {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    z-index: -1;
+}
 
 
-<style scoped></style>
+.image-container {
+    position: relative;
+}
+
+.image-container img {
+    position: absolute;
+    top: 0;
+    right: 0;
+    transition: transform 0.5s linear;
+
+}
+
+@keyframes slide-in {
+    0% {
+        right: 0;
+    }
+
+    100% {
+        transform: translateX(-250%);
+    }
+}
+
+.slide-in-image {
+    animation-name: slide-in;
+    animation-duration: 2.5s;
+    animation-fill-mode: forwards;
+}
+</style>
